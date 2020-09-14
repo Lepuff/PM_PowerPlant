4ce340236b02778ec7e156025ee24bbcaa6b8d04
Samuel <48945979+svsa1721@users.noreply.github.com>
2020-09-14 16:46:24 +0200

    Update arduino_RFID.ino

--- arduino_RFID/arduino_RFID.ino
+++ arduino_RFID/arduino_RFID.ino
@@ -1,3 +1,17 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
+
+#define RST_PIN 9
+#define SS_PIN 10
+
+byte readCard[4];
+String masterTag = "565EE2F7";
+String tagID = "";
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); //Parameters: (rs, enable, d4, d5, d6, d7)
+
 void setup() {
   // put your setup code here, to run once:
 
