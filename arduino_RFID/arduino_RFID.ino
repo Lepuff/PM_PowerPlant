e5ff8c9c9589b1cc712ee55c25b27dc1d0bd0ddf
Samuel <48945979+svsa1721@users.noreply.github.com>
2020-09-28 16:00:01 +0200

    code_cleaup

--- arduino_RFID/arduino_RFID.ino
+++ arduino_RFID/arduino_RFID.ino
@@ -10,23 +10,16 @@ const char LOGGEDOUT = '0';
 const char LOGGEDIN = '1';
 const char WARNING = '2';
 
-int buttonState;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
-int roomNumber = 0;
-int radiationLevel = 30;
-bool loginStatus = false;
-int firstChar;
+char firstChar;
 //#define RST_PIN 9
 //#define SS_PIN 10
 
 
 SoftwareSerial BTserial(A4, A3); //RX | TX
 byte readCard[4];
-String masterTag = "565EE2F7";
+//String masterTag = "565EE2F7";
 String tagID = "";
 bool loggedIn;
-
-//userInfo user;
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2); //Parameters: (rs, enable, d4, d5, d6, d7)
@@ -35,11 +28,7 @@ void setup() {
   SPI.begin(); //SPI bus
   mfrc522.PCD_Init(); //MFRC522 (RFID)
 
-  // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT);
-
   lcd.begin(16, 2); //LCD screen
-
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(" Scan Your Card ");
@@ -49,33 +38,29 @@ void setup() {
 }
 
 void loop() {
-
   lcd.clear();
   lcd.print(" Scan Your Card ");
-  //firstChar = BTserial.readString().charAt(0);
 
   //Wait until new tag is available
   while (getID()) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-
     BTserial.print(tagID);
     Serial.print(tagID);
-    BTserial.read();
+    //BTserial.read();
   }
 
   if (BTserial.available()) {
     lcd.clear();
     lcd.setCursor(0, 0);
     firstChar = BTserial.readString().charAt(0);
-    switch (firstChar) {
-      case LOGGEDOUT: //user not logged in -> login user
+
+    switch (firstChar) { //state machine for receiving messages via bluetooth
+      case LOGGEDOUT: //user is not logged in -> login user
         lcd.print(" Logged in");
         Serial.println(BTserial.readString().charAt(0)); //for debugging
         delay(1000);
         break;
 
-      case LOGGEDIN: //user logged in -> logout user
+      case LOGGEDIN: //user is logged in -> logout user
         lcd.print(" Logged out");
         Serial.println(BTserial.readString().charAt(0)); //for debugging
         delay(1000);
@@ -92,28 +77,6 @@ void loop() {
         break;
     }
   }
-
-  /*if (BTserial.available()) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    firstChar = BTserial.readString().charAt(0);
-
-    if (firstChar == LOGGEDOUT) { //user not logged in -> login user
-      lcd.print(" Logged in");
-      Serial.println(BTserial.readString().charAt(0));
-    }
-    if (firstChar == LOGGEDIN) { //user logged in -> logout user
-      lcd.print(" Logged out");
-      Serial.println(BTserial.readString().charAt(0));
-    }
-    if (firstChar == WARNING){
-      lcd.print("WARNING");
-      lcd.setCursor(0, 1);
-      lcd.print("LIMIT REACHED");
-    }
-    delay(1000);
-    }*/
-
 }
 
 //Read new tag if available
