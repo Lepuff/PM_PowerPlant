c925de2c2f5038811490f37a180732612f3ffbc8
Samuel <48945979+svsa1721@users.noreply.github.com>
2020-09-29 12:32:22 +0200

    communication protocol updated

--- arduino_RFID/arduino_RFID.ino
+++ arduino_RFID/arduino_RFID.ino
@@ -41,6 +41,7 @@ void loop() {
   while (getID()) {
     BTserial.print(tagID);
     Serial.print(tagID);
+    delay(500);
   }
 
   if (BTserial.available()) {
@@ -84,10 +85,11 @@ boolean getID()
   if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue
     return false;
   }
-  tagID = "1";
+  tagID = "";
   for ( uint8_t i = 0; i < 4; i++) { // The MIFARE PICCs that we use have 4 byte UID
     tagID.concat(String(mfrc522.uid.uidByte[i], HEX)); // Adds the 4 bytes in a single String variable
   }
+  tagID.concat(".");
   tagID.toUpperCase();
   mfrc522.PICC_HaltA(); // Stop reading
   return true;
